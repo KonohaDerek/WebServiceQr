54bf0976371a498f4ff988db44df6292a29e2783
KonohaDerek <peter890701@gmail.com>
2017-12-05 19:05:51 +0800

    init

---
+++ MvcApplication1/Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using MvcApplication1.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class MessageController : Controller
+    {
+        //實做Service的物件
+        MessageDBService data = new MessageDBService();
+
+        //
+        // GET: /Message/
+
+        public ActionResult Index()
+        {
+            return View(data.GetData());
+        }
+
+        public ActionResult Create() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(String Article_title,String Content) {
+            //呼叫Service中的DBCreate方法
+            data.DBCreate(Article_title, Content);
+            //導向指定的Action
+            return RedirectToAction("Index");
+
+
+
+
+        }
+
+
+    }
+}
---
+++ MvcApplication1/Service/MessageDBService.cs
@@ -0,0 +1,34 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Service
+{
+    public class MessageDBService
+    {
+        //實做資料庫模型
+        public MvcApplication1.Models.messageEntities db = new Models.messageEntities();
+
+        //此方法取得資料庫中，Article資料表並回傳
+        public List<Article> GetData() {
+            return db.Article.ToList();
+        }
+
+        //此方法將接收的資料存入資料庫
+        public void DBCreate(String Article_title , String Content) { 
+        
+            //實做資料表
+            Article NewData = new Article();
+            NewData.Title = Article_title;//此變數用於儲存文章標題
+            NewData.Content = Content;//此變數用於儲存內容標題
+            NewData.time = DateTime.Now;//此變數儲存文章發表時間
+
+            //新增一筆資料
+            db.Article.Add(NewData);
+            db.SaveChanges();
+        }
+
+    }
+}
